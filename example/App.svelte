b85fa8951e96279c5f5e5bae7b8b58e1373ba08e
Nikita Mostovoy <nik.mostovoy@gmail.com>
2019-10-08 17:08:14 +0300

    first try

---
+++ example/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Input from "./Input.svelte";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+  <h1>Hello CodeSandbox</h1>
+  <h2>Start editing to see some magic happen!</h2>
+  <Input alwaysShowMask maskChar="_" mask="0000-0000-0000-0000" />
+</main>
